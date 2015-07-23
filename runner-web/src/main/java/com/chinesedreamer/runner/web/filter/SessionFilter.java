9a6928b065646ed84afa7ce6998272be6fabadb7
Paris Tao <paristao1989@gmail.com>
2015-07-23 13:15:34 +0800

    base config

--- runner-web/src/main/java/com/chinesedreamer/runner/web/filter/SessionFilter.java
+++ runner-web/src/main/java/com/chinesedreamer/runner/web/filter/SessionFilter.java
@@ -1,10 +1,40 @@
 package com.chinesedreamer.runner.web.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
 /**
  * Description: 
  * @author Paris
  * @date Jul 23, 201510:51:28 AM
  * @version beta
  */
-public class SessionFilter {
+public class SessionFilter implements Filter{
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("------------------------>here");
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
